ded33b4e0eac8e9523215570b01aabee1fd44f39
taku-ting <ting.taku@gmail.com>
2024-08-13 18:19:06 +0900

    fix(oauth): support dynamic scheme

--- internal/api/handler/oauth2/login.go
+++ internal/api/handler/oauth2/login.go
@@ -1,20 +1,21 @@
 package oauth2
 
 import (
-	"fmt"
 	"net/http"
-	"path"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
-	providerName := chi.URLParam(r, "oauthProvider")
+func (h *Handler) Login(w http.ResponseWriter, req *http.Request) {
+	providerName := chi.URLParam(req, "oauthProvider")
 	oauth2, supported := h.OAuth2[providerName]
 	if !supported {
-		http.Redirect(w, r, "/.auth/login", http.StatusTemporaryRedirect)
+		http.Redirect(w, req, "/.auth/login", http.StatusTemporaryRedirect)
 	}
 
-	callbackURL := fmt.Sprintf("http://%s%s/callback", r.Host, path.Dir(r.URL.String()))
-	http.Redirect(w, r, oauth2.AuthCodeURL(callbackURL), http.StatusTemporaryRedirect)
+	if req.URL.Scheme == "" {
+		req.URL.Scheme = "http"
+	}
+	callbackURL := req.URL.Scheme + "://" + req.Host + "/.auth/" + providerName + "/callback"
+	http.Redirect(w, req, oauth2.AuthCodeURL(callbackURL), http.StatusTemporaryRedirect)
 }
